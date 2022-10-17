57f769aa92638dc9cca83d7c787304f45b15eca3
Stephen Tollenaar <stephen.tollenaar@radient360.com>
2022-10-17 12:59:45 -0230

    Refactoring markov references

--- lib/markovCommand/markovUrl.go
+++ lib/markovCommand/markovUrl.go
@@ -9,20 +9,28 @@ import (
 
 // MarkovURLCommand create a markov chain from an URL
 func MarkovURLCommand(interaction *discordgo.InteractionCreate, url string) {
-	u, err := URL.ParseRequestURI(url)
-
+	generated, err := GetMarkovURL(url)
 	if err != nil {
 		INVALID_URL_RESPONSE := "Not a valid URL was provided"
 		Bot.InteractionResponseEdit(interaction.Interaction, &discordgo.WebhookEdit{
 			Content: &INVALID_URL_RESPONSE,
 		})
-		return
+	} else {
+		Bot.InteractionResponseEdit(interaction.Interaction, &discordgo.WebhookEdit{
+			Content: &generated,
+		})
 	}
+}
 
+// GetMarkovURL creating a markov chain from a provided url
+func GetMarkovURL(url string) (string, error) {
+	u, err := URL.ParseRequestURI(url)
+
+	if err != nil {
+		return "", err
+	}
 	markov := markov.New()
 
 	generated := markov.ReadURL(u.String())
-	Bot.InteractionResponseEdit(interaction.Interaction, &discordgo.WebhookEdit{
-		Content: &generated,
-	})
+	return generated, nil
 }
