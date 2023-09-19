85f41774c0c51210ce38aa2ed160b0e37944d1a2
Stephen Tollenaar <stephen.tollenaar@radient360.com>
2023-09-19 18:54:24 -0230

    expanding request sending

--- internal/util/discord.go
+++ internal/util/discord.go
@@ -9,21 +9,32 @@ import (
 )
 
 const (
-	URL = "https://discord.com/api/v10/interactions/%s/%s/callback"
+	POST_URL  = "https://discord.com/api/v10/interactions/%s/%s/callback"
+	PATCH_URL = "https://discord.com/api/v10/webhooks/%s/%s/messages/@original"
 )
 
-func SendRequest(interactionID, interactionToken string, data []byte) {
+func SendRequest(method, interactionID, interactionToken string, data []byte) error {
 	// Create a HTTP post request
-	req, err := http.NewRequest("POST", fmt.Sprintf(URL, interactionID, interactionToken), bytes.NewBuffer(data))
+	var req *http.Request
+	var err error
+	switch method {
+	case "POST":
+		req, err = http.NewRequest("POST", fmt.Sprintf(POST_URL, interactionID, interactionToken), bytes.NewBuffer(data))
+	case "PATCH":
+		req, err = http.NewRequest("PATCH", fmt.Sprintf(PATCH_URL, interactionID, interactionToken), bytes.NewBuffer(data))
+	}
 	req.Header.Add("Content-Type", "application/json")
 	if err != nil {
 		fmt.Println(err)
+		return err
 	}
 	client := &http.Client{}
 	_, err = client.Do(req)
 	if err != nil {
 		fmt.Println(err)
+		return err
 	}
+	return nil
 }
 
 // Verifying the signature
