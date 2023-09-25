a28e40aa8374de6a846608aaac6d23f68ee04f04
Stephen Tollenaar <stephen@tollenaar.com>
2023-09-25 09:44:47 -0230

    Adding registering commands tool

---
+++ tools/register/register.go
@@ -0,0 +1,66 @@
+package main
+
+import (
+	"encoding/json"
+	"flag"
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/bwmarrin/discordgo"
+	"github.com/stollenaar/copypastabotv2/internal/util"
+)
+
+var (
+	bot            *discordgo.Session
+	commands       []*discordgo.ApplicationCommand
+	RemoveCommands = flag.Bool("rmcmd", false, "Remove all commands after shutdowning or not")
+)
+
+func init() {
+	c, err := os.ReadFile("commands.json")
+	if err != nil {
+		log.Fatal(err)
+	}
+	err = json.Unmarshal(c, &commands)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func init() {
+	flag.Parse()
+
+	token, err := util.ConfigFile.GetDiscordToken()
+	if err != nil {
+		log.Fatal(err)
+	}
+	bot, err = discordgo.New("Bot " + token)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func main() {
+	if *RemoveCommands {
+		cmds, err := bot.ApplicationCommands(bot.State.User.ID, "")
+		if err != nil {
+			log.Panicf("Cannot retrieve commands: %v", err)
+		}
+		for _, cmd := range cmds {
+			err := bot.ApplicationCommandDelete(bot.State.User.ID, "", cmd.ID)
+			if err != nil {
+				log.Panicf("Cannot delete '%v' command: %v", cmd.Name, err)
+			}
+		}
+		fmt.Println("Successfully removed all commands")
+	} else {
+		for _, v := range commands {
+			_, err := bot.ApplicationCommandCreate(bot.State.User.ID, "", v)
+			if err != nil {
+				log.Panicf("Cannot create '%v' command: %v", v.Name, err)
+			}
+		}
+		fmt.Println("Successfully created all commands")
+	}
+}
