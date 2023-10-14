04518f6dd791bbc508ffa69019071c9f0bb14e86
Stephen Tollenaar <stephen@tollenaar.com>
2023-10-14 18:27:05 -0230

    updating files for the speak command

--- terraform/main.tf
+++ terraform/main.tf
@@ -25,17 +25,6 @@ locals {
   name         = "copypastabot"
   used_profile = data.awsprofiler_list.list_profiles.profiles[try(index(data.awsprofiler_list.list_profiles.profiles.*.name, "personal"), 0)]
 
-  # TODO: See if this can be split up
-  environment_variables = {
-    AWS_PARAMETER_DISCORD_TOKEN        = "/discord_tokens/${local.name}"
-    AWS_PARAMETER_PUBLIC_DISCORD_TOKEN = "/discord_tokens/${local.name}_public"
-    AWS_PARAMETER_REDDIT_USERNAME      = "/reddit/username"
-    AWS_PARAMETER_REDDIT_PASSWORD      = "/reddit/password"
-    AWS_PARAMETER_REDDIT_CLIENT_ID     = "/reddit/client_id"
-    AWS_PARAMETER_REDDIT_CLIENT_SECRET = "/reddit/client_secret"
-    STATSBOT_URL                       = "statisticsbot"
-  }
-
   functions = {
     browse = {
       description                    = "Browse command for CopypastaBot"
@@ -178,10 +167,10 @@ locals {
       memory_size                    = 128
       layers                         = []
       reserved_concurrent_executions = -1
-      extra_permissions              = [data.aws_iam_policy_document.lambda_execution_role_policy_document.json, data.aws_iam_policy_document.sqs_role_policy_document.json, data.aws_iam_policy_document.speak_sqs_role_policy_document.json]
+      extra_permissions              = [data.aws_iam_policy_document.lambda_execution_role_policy_document.json, data.aws_iam_policy_document.sqs_role_policy_document.json, data.aws_iam_policy_document.speak_sqs_role_policy_document.json, data.aws_iam_policy_document.chat_sqs_role_policy_document.json]
       environment_variables = {
         AWS_SQS_URL       = data.terraform_remote_state.statisticsbot.outputs.sqs.request.url
-        AWS_SQS_URL_OTHER = aws_sqs_queue.speak_request.url
+        AWS_SQS_URL_OTHER = "${aws_sqs_queue.speak_request.url};${aws_sqs_queue.chat_request.url}"
       }
     }
     speakInterrupt = {
